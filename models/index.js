7488e2695a354d5a16e7d0c2f728c8899fa29220
Dane Cronin <danecronin@gmail.com>
2022-10-07 10:05:13 -0600

    added post CRUD

--- models/index.js
+++ models/index.js
@@ -1,3 +1,15 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = {User};
+//Define model associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+//Make reverse association to above relationship
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+
+module.exports = {User, Post};
