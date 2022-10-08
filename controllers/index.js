57af70618f4edbe004b34fedf076c33da82df083
Dane Cronin <danecronin@gmail.com>
2022-10-07 23:28:55 -0600

    setup hardcoded homepage via HandlebarsJS

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const homeRoutes = require('./home-routes.js');
 
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 //catches any endpoints if they do not exist and returns 404 error
 router.use((req, res) => {
