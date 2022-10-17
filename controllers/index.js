f1f303f1515465f40bc85b07acaec93c00f49195
Dane Cronin <danecronin@gmail.com>
2022-10-17 10:03:03 -0600

    added comments to edit-post.handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,11 @@
 const router = require('express').Router();
-const homeRoutes = require('./home-routes.js');
 
-const apiRoutes = require('./api');
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-
-//catches any endpoints if they do not exist and returns 404 error
-router.use((req, res) => {
-    res.status(404).end();
-});
+router.use('/api', apiRoutes);
 
 module.exports = router;
